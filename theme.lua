ff18c378b39938e1bb4cbf2aaa0236ff0bd2e132
blastmaster <blastmaster@tuxcode.org>
2014-12-12 11:23:02 +0100

    clear paths

--- theme.lua
+++ theme.lua
@@ -39,12 +39,12 @@ theme.border_marked = "#000000"
 
 -- Display the taglist squares
 -- theme.taglist_squares_sel   = "/usr/local/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/home/blastmaster/.config/awesome/themes/default/layouts/square.png"
+theme.taglist_squares_unsel = dir .. "square.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/local/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = dir .. "submenu.png"
 theme.menu_height = 14
 theme.menu_width  = 100
 
@@ -54,36 +54,37 @@ theme.menu_width  = 100
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/local/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/local/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = dir .. "titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = dir .. "titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/local/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/local/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/local/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/local/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = dir .. "titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = dir .. "titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = dir .. "titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = dir .. "titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/local/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/local/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/local/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/local/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = dir .. "titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = dir .. "titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = dir .. "titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = dir .. "titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/local/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/local/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/local/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/local/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = dir .. "titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = dir .. "titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = dir .. "titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = dir .. "titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/local/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/local/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/local/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/local/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = dir .. "titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = dir .. "titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = dir .. "titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = dir .. "titlebar/maximized_focus_active.png"
 
 theme.wallpapers = {
     {"/home/blastmaster/pictures/nasa2_1024-768.jpg", maximize=true},
     {"/home/blastmaster/pictures/debian-black-white.png", maximize=true},
     {"/home/blastmaster/pictures/tux.png", maximize=true},
-    {"/home/blastmaster/pictures/green-glider.png", maximize=true},
-    {"/home/blastmaster/pictures/dtux.png", maximize=true},
-    {"/home/blastmaster/pictures/c3d2_blue.png", maximize=true},
+	{"/home/blastmaster/pictures/proc-gchq.jpg", maximize=true},
+	{"/home/blastmaster/pictures/green-glider.png", maximize=true},
+	{"/home/blastmaster/pictures/dtux.png", maximize=true},
+	{"/home/blastmaster/pictures/c3d2_blue.png", maximize=true},
 }
 
 -- setting random default wallpaper
@@ -91,18 +92,18 @@ idx = math.random(1, table.getn(theme.wallpapers))
 theme.wallpaper = theme.wallpapers[idx][1]
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/home/blastmaster/.config/awesome/themes/default/layouts/fairho.png"
-theme.layout_fairv = "/home/blastmaster/.config/awesome/themes/default/layouts/fairvo.png"
-theme.layout_floating  = "/home/blastmaster/.config/awesome/themes/default/layouts/floatingo.png"
-theme.layout_magnifier = "/home/blastmaster/.config/awesome/themes/default/layouts/magnifiero.png"
-theme.layout_max = "/home/blastmaster/.config/awesome/themes/default/layouts/maxo.png"
-theme.layout_fullscreen = "/home/blastmaster/.config/awesome/themes/default/layouts/fullscreeno.png"
-theme.layout_tilebottom = "/home/blastmaster/.config/awesome/themes/default/layouts/tilebottomo.png"
-theme.layout_tileleft   = "/home/blastmaster/.config/awesome/themes/default/layouts/tilelefto.png"
-theme.layout_tile = "/home/blastmaster/.config/awesome/themes/default/layouts/tileo.png"
-theme.layout_tiletop = "/home/blastmaster/.config/awesome/themes/default/layouts/tiletopo.png"
-theme.layout_spiral  = "/home/blastmaster/.config/awesome/themes/default/layouts/spiralo.png"
-theme.layout_dwindle = "/home/blastmaster/.config/awesome/themes/default/layouts/dwindleo.png"
+theme.layout_fairh = dir .. "layouts/fairho.png"
+theme.layout_fairv = dir .. "layouts/fairvo.png"
+theme.layout_floating  = dir .. "layouts/floatingo.png"
+theme.layout_magnifier = dir .. "layouts/magnifiero.png"
+theme.layout_max = dir .. "layouts/maxo.png"
+theme.layout_fullscreen = dir .. "layouts/fullscreeno.png"
+theme.layout_tilebottom = dir .. "layouts/tilebottomo.png"
+theme.layout_tileleft   = dir .. "layouts/tilelefto.png"
+theme.layout_tile = dir .. "layouts/tileo.png"
+theme.layout_tiletop = dir .. "layouts/tiletopo.png"
+theme.layout_spiral  = dir .. "layouts/spiralo.png"
+theme.layout_dwindle = dir .. "layouts/dwindleo.png"
 
 theme.awesome_icon = "/usr/local/share/awesome/icons/awesome16.png"
 
