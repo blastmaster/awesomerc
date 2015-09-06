d153a95e3d6ff3587744e7c7a7d033fdd7a848fc
blastmaster <blastmaster@tuxcode.org>
2015-09-06 16:37:58 +0200

    add vicious
    add vicious
    
    delete vicious
    
    add vicious + some foo

--- rc.lua
+++ rc.lua
@@ -885,4 +885,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 redshift.init(1)
 -- }}}
-require("autostart")
+--require("autostart")
