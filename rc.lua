04c4da830e5e8b0c3db202bfd316b2397094dfc6
blastmaster <blastmaster@tuxcode.org>
2015-09-08 22:06:08 +0200

    switching layouts
    * pass layouts table to awful.layout.inc

--- rc.lua
+++ rc.lua
@@ -507,11 +507,11 @@ for s = 1, screen.count() do
     -- We need one layoutbox per screen.
     mylayoutbox[s] = awful.widget.layoutbox(s)
     mylayoutbox[s]:buttons(awful.util.table.join(
-	awful.button({ }, 1, function () awful.layout.inc(1) end),
+	awful.button({ }, 1, function () awful.layout.inc(awful.layout.layouts,1) end),
 	awful.button({ }, 2, function () mylayoutmenu:toggle() end),
-	awful.button({ }, 3, function () awful.layout.inc(-1) end),
-	awful.button({ }, 4, function () awful.layout.inc(1) end),
-	awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+	awful.button({ }, 3, function () awful.layout.inc(awful.layout.layouts,-1) end),
+	awful.button({ }, 4, function () awful.layout.inc(awful.layout.layouts,1) end),
+	awful.button({ }, 5, function () awful.layout.inc(awful.layout.layouts,-1) end)))
     -- Create a taglist widget
     mytaglist[s] = awful.widget.taglist(s, taglist_filter.call, mytaglist.buttons)
 
@@ -631,8 +631,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end, "decrement master"),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(1)  end, "Next Layout"),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1) end, "Previous Layout"),
+    awful.key({ modkey,           }, "space", function () awful.layout.inc(awful.layout.layouts, 1)  end, "Next Layout"),
+    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(awful.layout.layouts, -1) end, "Previous Layout"),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore, "restore window"),
 
