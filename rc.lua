93fd7664d93f4cd6faa82fd811ed92b6e8e72876
blastmaster <blastmaster@tuxcode.org>
2016-09-04 15:34:55 +0200

    work on keybindings
    * binding the keynumbers to tags live in rc/keybindings too.
    * a bit more structure in rc/keybindings

--- rc.lua
+++ rc.lua
@@ -324,56 +324,6 @@ root.buttons(awful.util.table.join(
 -- }}}
 globalkeys = require("rc.keybindings")
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it works on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 12 do
-    globalkeys.globalkeys = awful.util.table.join(globalkeys.globalkeys,
-        -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
-                function ()
-                        local screen = awful.screen.focused()
-                        local tag = screen.tags[i]
-                        if tag then
-                        tag:view_only()
-                        end
-                end,
-                {description = "view tag #"..i, group = "tag"}),
-        -- Toggle tag.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                function ()
-                    local screen = awful.screen.focused()
-                    local tag = screen.tags[i]
-                    if tag then
-                        awful.tag.viewtoggle(tag)
-                    end
-                end,
-                {description = "toggle tag #" .. i, group = "tag"}),
-        -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                function ()
-                    if client.focus then
-                        local tag = client.focus.screen.tags[i]
-                        if tag then
-                            client.focus:move_to_tag(tag)
-                        end
-                    end
-                end,
-                {description = "move focused client to tag #"..i, group = "tag"}),
-        -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                function ()
-                    if client.focus then
-                        local tag = client.focus.screen.tags[i]
-                        if tag then
-                            client.focus:toggle_tag(tag)
-                        end
-                    end
-                end,
-                {description = "toggle focused client on tag #" .. i, group = "tag"})
-    )
-end
-
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
