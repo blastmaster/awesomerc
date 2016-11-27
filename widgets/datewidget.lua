078471df2fbd1137564c406cb7380ddf904f21d2
blastmaster <blastmaster@tuxcode.org>
2016-11-27 13:46:59 +0100

    fix datewidget

--- widgets/datewidget.lua
+++ widgets/datewidget.lua
@@ -3,6 +3,6 @@ local wibox = require("wibox")
 local lain = require("lain")
 
 datewidget = wibox.widget.textclock('<span color="' .. beautiful.color_white .. '">%R</span> ')
-lain.widgets.calendar:attach(datewidget, { icons = '', font = beautiful.font, font_size = 8, fg = beautiful.widget_date_fg})
+lain.widgets.calendar:attach(datewidget, { font = beautiful.font, font_size = 8, fg = beautiful.widget_date_fg})
 
 return datewidget
