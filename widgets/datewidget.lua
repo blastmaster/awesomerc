c69d119dc84dd438ee900c19434be8306fa6ef02
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-02-18 17:58:01 +0100

    delete old widgets

--- widgets/datewidget.lua
+++
@@ -1,8 +0,0 @@
-local beautiful = require("beautiful")
-local wibox = require("wibox")
-local lain = require("lain")
-
-datewidget = wibox.widget.textclock('<span color="' .. beautiful.color_white .. '">%R</span> ')
-lain.widgets.calendar:attach(datewidget, { font = beautiful.font, font_size = 8, fg = beautiful.widget_date_fg})
-
-return datewidget
